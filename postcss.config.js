4d0270b556cff95a76b468bd3acb5ffeab045b04
sibiraj-sr <sibiraj.s@happyfox.com>
2020-06-07 11:20:59 +0530

    build: complete dependency update

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,9 @@
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
-const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
   plugins: [
     tailwindcss,
-    autoprefixer,
-    process.env.NODE_ENV === 'production' &&
-      purgecss({
-        content: ['./src/**/*.jsx', './src/**/*.html'],
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-      })
-  ].filter(Boolean)
+    autoprefixer
+  ]
 };
